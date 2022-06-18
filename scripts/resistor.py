41ced1f40c4d20fb7c219db7cf21671263efd1ad
Angelica <Angelica137@users.noreply.github.com>
2022-06-18 18:16:46 +0100

    refactor value var into reutn statement

--- scripts/resistor.py
+++ scripts/resistor.py
@@ -2,8 +2,7 @@ def resistor(colour: str) -> int:
     bands = ["black", "brown", "red", "orange", "yellow",
              "green", "blue", "violet", "grey", "white"]
     colours = colour.split("-")
-    value = str(bands.index(colours[0])) + str(bands.index(colours[1]))
-    return int(value)
+    return int(str(bands.index(colours[0])) + str(bands.index(colours[1])))
 
 
 """
